4ce7f46753fc74887217b67a8d2c693cfc5227ca
gdi2290 <github@gdi2290.com>
2016-04-27 17:43:08 -0700

    fix(universal): correct di

--- ts-webpack2-universal/main-server.ts
+++ ts-webpack2-universal/main-server.ts
@@ -4,11 +4,15 @@ import {Parse5DomAdapter} from '@igorminar/platform-server';
 Parse5DomAdapter.makeCurrent();
 
 
-import {provide, ComponentRef} from '@igorminar/core';
+import {provide, ComponentRef, enableProdMode} from '@igorminar/core';
+enableProdMode();
+
 import {DOCUMENT} from '@igorminar/platform-browser/src/dom/dom_tokens';
 import {DomSharedStylesHost, SharedStylesHost} from '@igorminar/platform-browser/src/dom/shared_styles_host';
 import {DomRootRenderer} from '@igorminar/platform-browser/src/dom/dom_renderer';
+import {RootRenderer} from '@igorminar/core/src/render/api';
 import {bootstrap} from '@igorminar/platform-browser-dynamic';
+import {ELEMENT_PROBE_PROVIDERS_PROD_MODE} from '@igorminar/platform-browser/src/dom/debug/ng_probe';
 
 
 import {
@@ -35,15 +39,12 @@ const document = `
 </html>
 `;
 
+
+import {AnimationBuilder} from '@igorminar/platform-browser/src/animate/animation_builder';
+import {EventManager} from '@igorminar/platform-browser/src/dom/events/event_manager';
+
 export function main() {
   return bootstrap(HelloWorldComponent, [
-
-      NodeSharedStylesHost,
-      provide(DomRootRenderer, {useClass: NodeDomRootRenderer_}),
-      provide(SharedStylesHost, {useExisting: NodeSharedStylesHost}),
-      provide(DomSharedStylesHost, {useExisting: NodeSharedStylesHost}),
-
-      provide(DomRootRenderer, {useClass: NodeSharedStylesHost}),
       provide(DOCUMENT, {
         useFactory: (domSharedStylesHost: DomSharedStylesHost) => {
           var doc: any = parseDocument(document);
@@ -51,7 +52,16 @@ export function main() {
           return doc;
         },
         deps: [DomSharedStylesHost]
-      })
+      }),
+
+      provide(DomRootRenderer, {useClass: NodeDomRootRenderer_}),
+      provide(RootRenderer, {useExisting: DomRootRenderer}),
+
+      NodeSharedStylesHost,
+      provide(SharedStylesHost, {useExisting: NodeSharedStylesHost}),
+      provide(DomSharedStylesHost, {useExisting: NodeSharedStylesHost}),
+
+      ELEMENT_PROBE_PROVIDERS_PROD_MODE
 
     ])
     .then((cmpRef: ComponentRef) => {
